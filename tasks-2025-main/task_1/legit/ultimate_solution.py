ed91e2577cf610291e8db083a893353a3b50a5cb
Aleksander Nowak <olek.nowak.20@gmail.com>
2025-03-16 09:58:01 +0100

    Broke ultimate_solution.py

--- tasks-2025-main/task_1/legit/ultimate_solution.py
+++ tasks-2025-main/task_1/legit/ultimate_solution.py
@@ -9,7 +9,6 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torchvision import models
 from typing import Tuple
-from torcheval.metrics import BinaryAUROC
 
 load_dotenv()
 
@@ -17,6 +16,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TOKEN = os.environ["ATHENA_TOKEN"]
 URL = "http://149.156.182.9:6060/task-1/submit"
 PRIVATE_DATASET_PATH = os.environ["PRIV_PATH"]
+MIA_CKPT_PATH = os.environ["01_MIA_69_PATH"]
 
 # Model atakujący (RMIA)
 class AttackModel(nn.Module):
@@ -63,12 +63,39 @@ class MembershipDataset(TaskDataset):
     def __getitem__(self, index) -> Tuple[int, torch.Tensor, int, int]:
         id_, img, label = super().__getitem__(index)
         return id_, img, label
+    
+class PrivateDataset(TaskDataset):
+    def __init__(self, victim_model, dataset):
+        self.victim_model = victim_model
+        self.dataset = dataset
+        self.features = []
+        self._prepare_data()
+
+    def _prepare_data(self):
+        dataloader = DataLoader(self.dataset, batch_size=1, shuffle=False)
+        tensor_list = []
+        self.victim_model.eval()
+        for _, img, label in dataloader:
+            img = img.to(DEVICE)
+            with torch.no_grad():
+                output = self.victim_model(img)
+            temp = torch.cat((torch.flatten(img), torch.flatten(output), label.to(DEVICE).to(torch.float32)))
+            tensor_list.append(temp)
+
+        self.features = torch.stack(tensor_list, dim=0)
 
 
 torch.serialization.add_safe_globals([MembershipDataset])
+torch.serialization.add_safe_globals([PrivateDataset])
 
 def inference_dataloader(dataset: MembershipDataset, batch_size):
     return torch.utils.data.DataLoader(dataset, batch_size, shuffle=False)
+
+def load_tgt_model():
+    victim_model = models.resnet18().to(DEVICE)
+    victim_model.fc = torch.nn.Linear(512, 44).to(DEVICE)
+    victim_model.load_state_dict(torch.load(MIA_CKPT_PATH, map_location=DEVICE))
+    return victim_model
 
 def random_random():
     dataset: MembershipDataset = torch.load(PRIVATE_DATASET_PATH)
@@ -88,15 +115,19 @@ def membership_prediction(model):
 
     if model is None: return random_random()
 
-    dataset: MembershipDataset = torch.load(PRIVATE_DATASET_PATH, map_location=DEVICE)
-    dataloader = inference_dataloader(dataset, 1)
+    model.eval()
+    dataset = torch.load(PRIVATE_DATASET_PATH, map_location=DEVICE)
+    tgt_model = load_tgt_model()
+    priv_dataset = PrivateDataset(tgt_model, dataset)
+    dataloader = DataLoader(dataset, 1)
     outputs_list = []
 
-    for _, img, _ in dataloader:
-        img = img.to(DEVICE)
+    for feature in dataloader:
+        print(feature)
+        feature = feature.to(DEVICE)
 
         with torch.no_grad():
-            membership_output = model(img)
+            membership_output = model(feature)
 
         outputs_list += membership_output.tolist()
 
